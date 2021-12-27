15c86d4b56f8e61ecf60ecddad5abc28610ad693
bongjun-kim94 <mydlwm_s@naver.com>
2021-12-27 20:01:23 +0900

    1227 props

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -6,21 +6,25 @@
 
 // css : 인라인 스타일, index.css&app.css 사용
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function Hello() {
-    const [name, setName] = useState('Mike');
+  const [name, setName] = useState("Mike");
+  const msg = age > 19 ? "성인입니다." : "미성년자 입니다.";
 
-    const changeName = () => {
-        // document.querySelector('#name').innerHTML = name;
-        setName((name === 'Mike' ? 'Jane' : 'Mike'));
-    }
+  const changeName = () => {
+    // document.querySelector('#name').innerHTML = name;
+    setName(name === "Mike" ? "Jane" : "Mike");
+  };
 
-    return (
-        <>
-            <h1>state</h1>
-            <h2 id='name'>{name}</h2>
-            <button onClick={changeName}>클릭</button>
-        </>
-    );
+  return (
+    <>
+      <h2 id="name">
+        {name}({age}) : {msg}
+      </h2>
+      <UserName name={name} />
+      <h2 id="name">{name}</h2>
+      <button onClick={changeName}>클릭</button>
+    </>
+  );
 }
